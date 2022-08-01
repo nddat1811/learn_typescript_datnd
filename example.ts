3247dce67a7fe3c170e8debf8e8228be0fa8cd6c
datnd <datnd@hybrid-technologies.vn>
2022-08-01 09:08:01 +0700

    type sth

---
+++ example.ts
@@ -0,0 +1,20 @@
+class Vehicle {
+    name: string;
+    brand: string;
+    constructor (name: string, brand: string) {
+        this.name = name;
+        this.brand = brand;
+    }
+
+    info() {
+      return "Name: " + this.name + ", brand: " + this.brand;
+    }
+
+    start() {
+        return this.brand + " " + this.name + " is runing.";
+    }
+}
+
+let bugati = new Vehicle("Veyon", "Bugati");
+let t = bugati.start();
+console.log(t)
